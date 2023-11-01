cf8873dc263d6a61b8c3ca70c026dfc7920eccfe
mlgtcode <107129718+mlgtcode@users.noreply.github.com>
2023-11-01 19:35:59 +0100

    Neu: client_quotations.php

---
+++ language/de_de/client_quotations.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Language definitions. Blesta German Translation Project.
+ * 
+ * @package blesta
+ * @github https://github.com/BlestaStore/blesta.translation-DE-inofficial
+ * @copyright Copyright (c) 2023, Phillips Data, Inc.
+ * @license https://www.blesta.com/license/ The Blesta License Agreement
+ * @link https://www.blesta.com/ Blesta
+ */
+
+// Fehler
+$lang['ClientQuotations.!error.password_mismatch'] = 'Falsches Passwort!';
+
+
+// Erfolg
+$lang['ClientQuotations.!success.approved_quotation'] = 'Das Angebot wurde akzeptiert!';
+
+
+// Index
+$lang['ClientQuotations.index.page_title'] = 'Kunde #%1$s Angebote'; // %1$s is the client ID number
+
+$lang['ClientQuotations.index.category_pending'] = 'Offen';
+$lang['ClientQuotations.index.category_approved'] = 'Akzeptiert';
+$lang['ClientQuotations.index.category_expired'] = 'Abgelaufen';
+
+$lang['ClientQuotations.index.boxtitle_quotations'] = 'Angebote';
+$lang['ClientQuotations.index.heading_quotation'] = 'Angebot #';
+$lang['ClientQuotations.index.heading_title'] = 'Beschreibung';
+$lang['ClientQuotations.index.heading_subtotal'] = 'Zwischensumme';
+$lang['ClientQuotations.index.heading_total'] = 'Summe';
+$lang['ClientQuotations.index.heading_date_created'] = 'Erstellt';
+$lang['ClientQuotations.index.heading_date_expires'] = 'Ablaufdatum';
+$lang['ClientQuotations.index.heading_options'] = 'Options';
+$lang['ClientQuotations.index.option_view'] = 'Ansehen';
+$lang['ClientQuotations.index.option_approve'] = 'Akzeptieren';
+
+$lang['ClientQuotations.index.no_results'] = 'Sie haben keine Anegbote.'; // %1$s is the language for the quotation category type
+
+
+// Akzeptieren
+$lang['ClientQuotations.approve.heading_approve'] = 'Akzeptieren';
+$lang['ClientQuotations.approve.field_password'] = 'Bitte geben Sie Ihr Passwort ein um das Angebot zu akzeptieren.';
+$lang['ClientQuotations.approve.field_cancel'] = 'Ablehnen';
+$lang['ClientQuotations.approve.field_submit'] = 'Akzeptieren';
+
+
+// Rechnungen
+$lang['ClientQuotations.invoices.heading_invoice'] = 'Proforma-Rechnung #';
+$lang['ClientQuotations.invoices.heading_amount'] = 'Summe';
+$lang['ClientQuotations.invoices.heading_paid'] = 'Bezahlt';
+$lang['ClientQuotations.invoices.heading_date_billed'] = 'Bezahlt';
+
+$lang['ClientQuotations.invoices.no_results'] = '-';
